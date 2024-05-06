534c4a322e501df992817585898f5ff7a7301bbe
harnya <gaurav.r.bole@gamil.com>
2024-05-06 12:04:13 +0530

    job and job history

--- flow_playgraound.py
+++ flow_playgraound.py
@@ -1,13 +1,12 @@
 
 from abc import ABC, abstractmethod
 from datetime import datetime
-from typing import Any, Callable, Collection, Dict, List, Literal, Mapping
+from typing import Any, Callable, Collection, Dict, List, Literal, Mapping,
 from pydantic import BaseModel, Field
 
 Status = Literal['success', 'progress', 'failed']
 
 STATUS_MAPPING = {'s': 'success', 'p': 'progress', 'f': 'failed'}
-
 
 class Workflow(BaseModel):
     """_summary_
@@ -33,11 +32,7 @@ class Job(BaseModel):
 
 
 class JobDependancy(BaseModel):
-    """_summary_
 
-    Args:
-        BaseModel (_type_): _description_
-    """
     job: int = Field(alias="job_id")
     upstream_job: int = Field(alias="upstream_job_id")
 
@@ -124,7 +119,7 @@ workflow_jobs = [
 wh: List[WorkflowHoistory] = []
 jh: List[JobHistory] = []
 x_con: List[Xcom] = []
-
+jd : JobDependancy =[]
 
 class WorkflowExection:
     """_summary_
@@ -148,6 +143,7 @@ class WorkflowExection:
     def __exit__(self, exc_type: str, exc_val: str, exc_tb: str):
         self.workflow_history.status = 'success'
         if exc_type or exc_val or exc_tb:
+            print(exc_type, exc_val, exc_tb)
             self.workflow_history.status = 'failed'
         wh.append(self.workflow_history)
         print(f"{' End workflow : ':*^100}")
@@ -156,25 +152,22 @@ class WorkflowExection:
 
 
 class JobExecutorTemplate(ABC):
-    """_summary_
 
-    Args:
-        ABC (_type_): _description_
-
-    Raises:
-        Exception: _description_
-    """
-    workflow_history: WorkflowHoistory | None = None
-    job_history: JobHistory | None = None
-    job_name = None
-    job_id = 0
-    f_args: Collection[Any] | None = None
-    f_kwargs: Mapping[str, Any] | None = None
-    status: Status = "success"
+    def __init__(self, workflow_history: WorkflowHoistory,
+                job_name: str,
+                exc_code: Callable[[], Any],
+                f_args: Collection[Any] | None = None,
+                f_kwargs: Mapping[str, Any] | None = None) -> None:
+        self.workflow_history = workflow_history
+        self.exc_code = exc_code
+        self.job_name = job_name
+        self.f_args = f_args or ()
+        self.f_kwargs = f_kwargs or {}
+        self.job_id = 0
+        self.status = "success"
 
     def job_start(self):
-        """_summary_
-        """
+
         print(f"{' job start ':#^100}")
 
         job = next((wj for wj in workflow_jobs if wj.workflow ==
@@ -186,110 +179,66 @@ class JobExecutorTemplate(ABC):
         self.job_history = job_history
 
     def job_end(self, status: Status = "success"):
-        """_summary_
 
-        Args:
-            status (Status, optional): _description_. Defaults to "success".
-
-        Raises:
-            Exception: _description_
-        """
         if not self.job_history:
             raise Exception("Job history not found")
         self.job_history.status = status
-        print("--------> status", status)
-        print(self.workflow_history)
-        self.workflow_history.status = status
-        print(self.workflow_history)
-
         jh.append(self.job_history)
+        if status == "failed":
+            raise Exception(f"job failed {self.job_history.id}")
         print(f"{' job end ':#^100}")
 
-    @classmethod
-    def execute(cls, workflow_history: WorkflowHoistory, job_name: str,
-                exc_code: Callable[[Any], Any],
-                f_args: Collection[Any] | None = None,
-                f_kwargs: Mapping[str, Any] | None = None):
-        """_summary_
-
-        Args:
-            workflow_history (WorkflowHoistory): _description_
-            job_name (str): _description_
-            exc_code (Callable[[Any], Any]): _description_
-            f_args (Collection[Any] | None, optional): _description_. Defaults to None.
-            f_kwargs (Mapping[str, Any] | None, optional): _description_. Defaults to None.
-        """
+    def execute(self):
         status: Status = "success"
-        instance = cls()
         try:
-            cls.workflow_history = workflow_history
-            cls.job_name = job_name
-            instance.job_start()
-            cls.f_args = f_args or ()
-            cls.f_kwargs = f_kwargs or {}
-            instance.execute_job(
-                workflow_history=workflow_history, exc_code=exc_code)
+            self.job_start()
+            self.execute_job()
 
         except Exception as e:
             print(e)
             status = "failed"
         finally:
-            instance.job_end(status=status)
+            self.job_end(status=status)
         # return instance
+ 
 
     @abstractmethod
-    def execute_job(self, workflow_history: WorkflowHoistory,
-                    exc_code: Callable[[Any], Any]) -> List[JobHistory]:
-        """_summary_
-
-        Args:
-            workflow_history (WorkflowHoistory): _description_
-            exc_code (Callable[[Any], Any]): _description_
-
-        Returns:
-            List[JobHistory]: _description_
-        """
+    def execute_job(self) -> List[JobHistory]:
         pass
 
     # https://python.plainenglish.io/building-microservices-with-fastapi-part-ii-a-microservice-application-with-fastapi-c190d57922ba
 
 
 class JobExecuter(JobExecutorTemplate):
-    """_summary_
+    def __init__(self, *args: (Any), **kwargs: Dict[str, Any]) -> None:
+        super().__init__(*args, **kwargs)
+        self.dependencies: List['JobExecuter'] = []
 
-    Args:
-        JobExecutorTemplate (_type_): _description_
+    def execute_job(self) -> List[JobHistory]:
 
-    Raises:
-        Exception: _description_
-
-    Returns:
-        _type_: _description_
-    """
-
-    # type: ignore
-    def execute_job(self, workflow_history: WorkflowHoistory,
-                    exc_code: Callable[[], Any]) -> List[JobHistory]:
-        """_summary_
-
-        Args:
-            workflow_history (WorkflowHoistory): _description_
-            exc_code (Callable[[], Any]): _description_
-
-        Raises:
-            Exception: _description_
-
-        Returns:
-            List[JobHistory]: _description_
-        """
         if not self.job_id:
             raise Exception("Job not started yet")
-        r: Any = exc_code(*self.f_args, **self.f_kwargs)
-        print(r)
+        r: Any = self.exc_code(*self.f_args, **self.f_kwargs)
         x_com = Xcom(id=1, data={"try": "hah"}, job_id=self.job_id,
-                     workflow_id=workflow_history.workflow, workflow_history_id=workflow_history.id)
+                     workflow_id=self.workflow_history.workflow, 
+                     workflow_history_id=self.workflow_history.id)
         x_con.append(x_com)
         return jh
+    
+    def upstream(self, *jobs: 'JobExecuter') -> None:
+        if self not in self.dependencies:
+            self.dependencies.append(self)
+        for job in jobs:
+            if job not in self.dependencies:
+                self.dependencies.append(job)
+    
+    def run_dependencies(self):
+        for job in self.dependencies:
+            print(job)
+            job.execute()
+
+    def __rshift__(self,  *jobs: 'JobExecuter'):
+        return self.upstream(*jobs)
 
 
 def first_job(a: int, b: int) -> Any:
@@ -302,15 +251,23 @@ def first_job(a: int, b: int) -> Any:
     Returns:
         Any: _description_
     """
+    print(a, b)
     print(a/b)
     return "in first python"
 
 
 with WorkflowExection(workflow_name="second") as we:
-    je = JobExecuter().execute(workflow_history=we, job_name="firstjob",
-                               exc_code=first_job, f_kwargs={"a": 100, "b": 200})
-    je = JobExecuter().execute(workflow_history=we, job_name="secon_job",
-                               exc_code=first_job, f_kwargs={"a": 600, "b": 0})
+    je1 = JobExecuter(we, "firstjob",
+                               first_job, f_kwargs={"a": 100, "b": 200})
+    je2 = JobExecuter(we, "secon_job", first_job, f_kwargs={"a": 600, "b": 10})
+    je3 = JobExecuter(we, "secon_job",first_job, f_kwargs={"a": 600, "b": 5})
+    # je1.upstream(je1)
+    # je1.upstream(je2)
+    print(je1.job_id, "----------------")
+
+    je1 >> je2
+    # je2 >> je3
+    je1.run_dependencies()
 
 
 print(jh)
